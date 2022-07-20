ab6c238818fee0b99f715d515f7204095e960ed5
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-07-20 19:55:41 -0400

    Initial commit

---
+++ salcode-share-desc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       salcode Share Description
+ * Plugin URI:        https://github.com/salcode/salcode-share-desc
+ * Description:       Allows you to set the Share Description in post meta from Gutenberg block editor.
+ * Version:           0.1.0
+ * Requires at least: 6.0.0
+ * Requires PHP:      7.4
+ * Author:            Sal Ferrarello
+ * Author URI:        https://salferrarello.com/
+ * License:           apache-2.0
+ * License URI:       http://www.apache.org/licenses/LICENSE-2.0
+ * Text Domain:       salcode-share-desc
+ * Domain Path:       /languages
+ */
