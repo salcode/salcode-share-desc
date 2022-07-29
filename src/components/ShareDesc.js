f0e0bb0206ea48a9c0de5efaf7d4f1e0958f8d73
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-07-29 11:08:42 -0400

    Add TextareaControl to ShareDesc component

--- src/components/ShareDesc.js
+++ src/components/ShareDesc.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
 
+import { TextareaControl } from '@wordpress/components';
+
 export default function ShareDesc({
   desc,
 }) {
   return (
     <p>
-      {desc}
+      <TextareaControl
+        value={desc}
+      />
     </p>
   );
 }
