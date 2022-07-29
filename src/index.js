fc9e6b93837fef4686d5ad8ffac28ad51a21703b
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-07-29 10:35:24 -0400

    JavaScript register plugin and render in sidebar
    Replace our stub console.log() statement with React JavaScript to render
    our message in the Document (a.k.a. Post) Settings sidebar.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('salcode javascript');
+import { registerPlugin } from '@wordpress/plugins';
+import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+
+registerPlugin(
+  'salcode-share-desc', // Unique identifier for our plugin.
+  {
+    render: () => {
+      return (
+        <PluginDocumentSettingPanel>
+          <p>Placeholder for future input field for share description</p>
+        </PluginDocumentSettingPanel>
+      );
+    },
+  },
+);
