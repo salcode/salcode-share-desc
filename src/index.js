875d42779cc3f7f9955b47f4960b211d9593872a
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-07-29 11:53:43 -0400

    Pass setShareDescription into ShareDesc as a prop

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ registerPlugin(
         >
           <ShareDesc
             desc={shareDescription}
+            setShareDescription={setShareDescription}
           />
         </PluginDocumentSettingPanel>
       );
