23d3deb00210f7a0a3cf37e82461b791893d8657
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-07-29 11:26:50 -0400

    Update ShareDesc with realtime post meta value
    As the post meta value changes in the editor, we re-render ShareDesc
    with the latest desc value.
    
    The function inside useSelect() is automatically fired everytime the
    data in WordPress changes (e.g. post meta or even the main body content)
    and the select is run to get the latest value.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { registerPlugin } from '@wordpress/plugins';
+import { useSelect } from '@wordpress/data';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 import ShareDesc from './components/ShareDesc';
@@ -7,12 +8,20 @@ registerPlugin(
   'salcode-share-desc', // Unique identifier for our plugin.
   {
     render: () => {
+      const shareDescription = useSelect(
+        (select) => {
+          return select('core/editor')
+            .getEditedPostAttribute('meta')
+            .salcode_share_desc;
+        }
+      );
+
       return (
         <PluginDocumentSettingPanel
           title={__('Social Share', 'salcode-share-desc')}
         >
           <ShareDesc
-            desc="This is my stub description, ultimately this value will come from post meta."
+            desc={shareDescription}
           />
         </PluginDocumentSettingPanel>
       );
